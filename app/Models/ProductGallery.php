4a4ffd3959785da2df3f7341fc6302377185a4bf
Yusuf  Maruf <achmadusufalmaruf@gmail.com>
2024-05-18 23:04:06 +0700

    first commit

---
+++ app/Models/ProductGallery.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
+
+class ProductGallery extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'url',
+        'products_id',
+    ];
+
+    public function getUrlAttribute($url)
+    {
+        return config('app.url') . Storage::url($url);
+    }
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'products_id', 'id');
+    }
+}
---
+++ app/Models/TransactionItem.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TransactionItem extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'users_id',
+        'products_id',
+        'transaction_id',
+        'quantity',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'users_id', 'id');
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'products_id', 'id');
+    }
+
+    public function transaction()
+    {
+        return $this->belongsTo(Transaction::class, 'transaction_id', 'id');
+    }
+}
